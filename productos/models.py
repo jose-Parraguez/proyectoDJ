ee2594fe4843c93a951f8711aaa8a2af15c010ab
jose-parraguez <joseadanparraguez@gmail.com>
2023-12-16 12:03:36 -0300

    modelo

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    #CASCADE = eliminar el producto 
+    #PROTECT = lanza un error 
+    #RESTRICT = solo elimina si no existe un producto 
+    #SET_NULL = actualiza a valor nulo 
+    #SET_DEFAULT = asigna valor por defecto 
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE )
+
+
+

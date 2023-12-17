14915e3b408fd68d6bd4d738f7c227932e001e1f
jose-parraguez <joseadanparraguez@gmail.com>
2023-12-16 21:49:01 -0300

    ajustes de modelo

--- productos/models.py
+++ productos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Categoria(models.Model):
@@ -14,7 +15,11 @@ class Producto(models.Model):
     #RESTRICT = solo elimina si no existe un producto 
     #SET_NULL = actualiza a valor nulo 
     #SET_DEFAULT = asigna valor por defecto 
-    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE )
+    categoria = models.ForeignKey(
+        Categoria,   
+        on_delete = models.CASCADE 
+        )
 
+    creado_en = models.DateTimeField(default = timezone.now)
 
 

3d80b0c8768143e9da55ac75acbcc1c1a46dae92
jose-parraguez <joseadanparraguez@gmail.com>
2023-12-18 09:25:24 -0300

    actualizo el modelo y la vista de la base de datos

--- productos/admin.py
+++ productos/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Categoria,Producto
 # Register your models here.
 
-admin.site.register(Categoria)
-admin.site.register(Producto)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("id","nombre")
+
+class ProductoAdmin(admin.ModelAdmin):
+    exclude = ('creado_en', )#para excluir un producto de la tabla en este caso es la fecha
+    list_display = ("nombre","stock","creado_en")
+
+admin.site.register(Categoria,CategoriaAdmin)
+admin.site.register(Producto,ProductoAdmin)
--- productos/models.py
+++ productos/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
@@ -22,4 +25,5 @@ class Producto(models.Model):
 
     creado_en = models.DateTimeField(default = timezone.now)
 
-
+    def __str__(self):
+        return self.nombre
